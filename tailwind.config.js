3ae366d0a43863fec024441e1f6ef46aec65335c
tony-42069 <dsadellari@abizadvisors.com>
2024-12-14 13:30:32 -0500

    Fixed Next.js setup with basic configuration and Hello World page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        purple: {
-          950: '#1a0930',
-        },
-        pink: {
-          400: '#f078b1',
-        },
-      },
+        brand: {
+          darkRed: '#5d002c',
+          pink: '#f078b1',
+          purple: '#d7b7ff',
+          lime: '#b7ffc3',
+          light: '#fcebdb'
+        }
+      }
     },
   },
   plugins: [],
